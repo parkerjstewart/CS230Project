9dcba9cd67e8f89039758dd875ed216ac6e3888a
EC2 Default User <ec2-user@ip-172-31-24-66.ec2.internal>
2024-11-18 10:23:24 +0000

    standardizes labels for dataloader

--- code/data/ImageDataset.py
+++ code/data/ImageDataset.py
@@ -4,7 +4,7 @@ from PIL import Image
 from torch.utils.data import Dataset, DataLoader
 import torchvision.transforms as transforms
 
-
+'''
 class ImageDataset(Dataset):
     def __init__(self, annotations_path, images_dir, transform=None):
         self.images_dir = images_dir
@@ -23,7 +23,7 @@ class ImageDataset(Dataset):
         
         for annotation in annotations['annotations']:
             image_id = annotation['image_id']
-            category_id = annotation['category_id']
+            category_id = int(annotation['category_id'])
             file_name = os.path.basename(image_metadata[image_id]['file_name'])
             self.image_to_label[file_name] = category_id
         
@@ -50,11 +50,62 @@ class ImageDataset(Dataset):
         label = self.category_mapping[category_id]
         
         return image, label    
-
 '''
 
+class ImageDataset(Dataset):
+    def __init__(self, annotations_path, images_dir, transform=None):
+        self.images_dir = images_dir
+        self.annotations_path = annotations_path
+        self.transform = transform
+
+        with open(annotations_path, 'r') as f:
+            annotations = json.load(f)
+
+        # label 0 will be real and label 1 will be fake
+        categories = annotations["categories"]
+        category_ids_to_name = {category["id"] : category["name"] for category in categories}
+
+        self.image_ids_to_file_names = {}
+        for image in annotations["images"]:
+            id_num = image["id"]
+            file_name = os.path.basename(image["file_name"])
+            self.image_ids_to_file_names[id_num] = file_name
+        
+        self.image_ids_to_labels = {}
+        for image in annotations["annotations"]:
+            id_num = image["image_id"]
+            category_id = image["category_id"]
+            if category_ids_to_name[category_id] == "Real":
+                self.image_ids_to_labels[id_num] = 0
+            else:
+                self.image_ids_to_labels[id_num] = 1 
+        
+        self.image_ids = list(self.image_ids_to_labels.keys())
+
+    def __len__(self):
+        return len(self.image_ids)
+
+    def __getitem__(self, idx):
+        # Get image filename
+        file_name = self.image_ids_to_file_names[self.image_ids[idx]]
+        image_path = os.path.join(self.images_dir, file_name)
+        
+        # Load image
+        image = Image.open(image_path).convert("RGB")
+        
+        # Apply transformations
+        if self.transform:
+            image = self.transform(image)
+        
+        # Get label
+        label = self.image_ids_to_labels[self.image_ids[idx]]
+        
+        return image, label    
+
+'''
 # takes in "test", "val", "test-dev", or "test"
 # returns the json object for that dataset
+
 def load_annotations(dataset):
     annotations_path = "../../data/annotations"
     mappings = {"test": "/home/ec2-user/CS230Project/data/annotations/Test-Challenge_poly.json",
